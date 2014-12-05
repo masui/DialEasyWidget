c2e3471aeb5e01f1dee24e6e94a2b84228e3114b
Toshiyuki Masui <masui@pitecan.com>
2014-12-06 01:25:03 +0900

    DialEasyService.javaとか要らなかったみたい

--- src/com/pitecan/dialeasy/DialEasy.java
+++ src/com/pitecan/dialeasy/DialEasy.java
@@ -1,8 +1,15 @@
 package com.pitecan.dialeasy;
+
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+
+import android.widget.RemoteViews;
+
+import android.net.Uri;
+import android.util.Log;
 
 //ホームスクリーンウィジェットを提供するブロードキャストレシーバー
 public class DialEasy extends AppWidgetProvider {
@@ -11,8 +18,15 @@ public class DialEasy extends AppWidgetProvider {
     public void onUpdate(Context context,
         AppWidgetManager appWidgetManager,int[] appWidgetIds) {
 
-        //ホームスクリーンウィジェットのイベント処理を担当するサービスの起動
-        Intent intent=new Intent(context,DialEasyService.class);
-        context.startService(intent);
+        //リモートビューの生成
+        RemoteViews view=new RemoteViews(context.getPackageName(),R.layout.dialeasy);
+
+	Intent intent = new Intent(Intent.ACTION_CALL);
+	intent.setData(Uri.parse("tel:"+"0467235838")); 
+	PendingIntent pending = PendingIntent.getActivity(context, 0, intent, 0);
+	view.setOnClickPendingIntent(R.id.imagebutton, pending);
+
+        //ホームスクリーンウィジェットの画面更新
+	appWidgetManager.updateAppWidget(appWidgetIds, view);
     }
 }
